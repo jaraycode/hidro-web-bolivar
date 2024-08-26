65af40c7fb41b7bd403c24a80b2c8f0f48829596
angel andarte <angelandarte15@gmail.com>
2024-08-26 12:19:17 -0400

    Agregar iconos al footer.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,26 @@
 import HomeNavbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 export default function Home() {
   return (
-    <div>
+    
+    <div className='main'>
       <HomeNavbar/>
-        Home jom
+
+      <div className='mainDiv border-2 border-red-800  w-full'>
+        
+          <h1 className='bigTitle'>:c </h1>
+          <div className='research'>1234</div>
+          <div className='line'/>
+          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faTwitter} />
+      </div>
+
       <Footer/>
     </div>
   );
 }
+
--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,48 +1,62 @@
 import React, {Component} from 'react';
 //import { social_icons } from 'react-social-icons';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAmazon, faFacebook, faInstagram, faMailchimp, faTwitter, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
         const current_year = new Date();
         return (
-            <div id="home-footer" className='bg-cyan-gradient flex flex-row gap-4 justify-center' >
+            <div className='home-footer bg-cyan-gradient flex flex-row gap-4 justify-center' >
                 
                 <div className='home-footer-elements'>
-                    <h3>Redes sociales</h3>
-
-                    <h2 className='hover:text-white'>Facebook</h2>
-                    <h2 className='hover:text-white'>Instagram</h2>
-                    <h2 className='hover:text-white'>Twitter</h2>
+                    <h2 className='text-16-semibold'>Redes sociales</h2>
+                    <div className='flex justify-between gap-4'>
+                        <FontAwesomeIcon icon={faFacebook} size='xl'/>
+                        <FontAwesomeIcon icon={faInstagram} size='xl'/>
+                        <FontAwesomeIcon icon={faTwitter} size='xl'/>
+                    </div>
                 </div>
 
                 <div className='home-footer-elements'>
-                    <h2>Ultimas Noticias</h2>
+                    <h2 className='text-16-semibold'>Ultimas Noticias</h2>
 
-                    <p>Maduro chupalo.</p>
+                    <p className='text-14-regular'>Llego el agua.</p>
                 </div>
 
 
                 <div className='home-footer-elements'>
-                    <h2>Accesos directos</h2>
-                    <Link href='/' className='hover:text-white'>
+                    <h2 className='text-16-semibold'>Accesos directos</h2>
+                    <Link href='/' className='text-14-regular hover:text-white'>
                         Privacidad
                     </Link>
 
-                    <Link href='/' className='hover:text-white'>
+                    <Link href='/' className='text-14-regular hover:text-white'>
                         Terminos
                     </Link>
 
-                    <Link href='/' className='hover:text-white'>
+                    <Link href='/' className='text-14-regular hover:text-white'>
                         Servicios
                     </Link>
                 </div>
 
                 <div className='home-footer-elements'>
-                    <h2>Contacto</h2>
+                    <h2 className='text-16-semibold'>Contacto</h2>
                     <ul>
-                        <li>753Q+7JX, Trans. B, Ciudad Guayana 8050, Bolívar</li>
-                        <li>+58 0286-7129800</li>
-                        <li>info@hidrobolivar.com</li>
+                        <div className='gap-1 flex flex-row'>
+                            <FontAwesomeIcon icon={faMailchimp} size='xl' className='p-1'/>
+                            <li className='text-14-regular'>753Q+7JX, Trans. B, Ciudad Guayana 8050, Bolívar</li>
+                        </div>
+                         
+                        <div className='gap-2 flex flex-row'>
+                            <FontAwesomeIcon icon={faWhatsapp} size='xl'/>
+                            <li className='text-14-regular'>+58 0286-7129800</li>
+                        </div>
+
+                        <div className='gap-2 flex flex-row'>
+                            <FontAwesomeIcon icon={faAmazon} size='xl'/>
+                            <li className='text-14-regular'>info@hidrobolivar.com</li>
+                        </div>
                     </ul>
                 </div>
       
